4bb34a393e7a50eae33f277c9e295ff547111c75
Michael Qin <1mike12@gmail.com>
2024-02-20 21:05:14 -0600

    load spatial functionality

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -1,3 +1,5 @@
+import {Knex} from "knex";
+
 require("ts-node/register")
 export default {
   client: 'sqlite3',
@@ -8,4 +10,17 @@ export default {
   migrations: {
     directory: './migrations'
   },
-}
+  pool: {
+    afterCreate: (conn, done) => {
+      conn.loadExtension('mod_spatialite', (err: Error) => {
+        if (err) {
+          console.error('Failed to load Spatialite extension:', err);
+          done(err, conn);
+        } else {
+          console.log('Spatialite extension loaded successfully.');
+          done(null, conn); // proceed with the connection
+        }
+      });
+    }
+  }
+} as Knex.Config
