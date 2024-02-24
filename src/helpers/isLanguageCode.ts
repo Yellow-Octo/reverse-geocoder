e9c3d00b0a3fb0ad027f9b4ec27183f1dfea72a8
Michael Qin <1mike12@gmail.com>
2024-02-23 18:45:37 -0600

    optimize isLanguageCode.ts

--- src/helpers/isLanguageCode.ts
+++ src/helpers/isLanguageCode.ts
@@ -1,4 +1,5 @@
 export function isLanguageCode(value: string): boolean {
+  if (value === null) return false
   const languageCodePattern = /^[a-z]{2,3}(-[A-Z]{2}|-[A-Za-z]+)?$/;
   return languageCodePattern.test(value);
 }
